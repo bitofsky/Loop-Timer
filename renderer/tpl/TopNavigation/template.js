b626a94393dc95cdd31f3900d1b64f6e0a05ddf5
Bum-Seok Hwang <bitofsky@naver.com>
2017-04-04 03:09:03 +0900

    remove js/map

--- renderer/tpl/TopNavigation/template.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.default = () => {
-    $('TopNavigation .collapse, TopNavigation .dropdown')
-        .on('shown.bs.collapse hidden.bs.collapse shown.bs.dropdown hidden.bs.dropdown', () => $(window).trigger('resize'));
-};
-//# sourceMappingURL=template.js.map
